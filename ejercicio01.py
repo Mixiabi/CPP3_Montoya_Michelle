2d7081087f3d2934139f27542c9843ffa8eed228
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-10-30 05:11:01 -0500

    Agregando Indice

--- ejercicio01.py
+++ ejercicio01.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 
+
 st.title('Ejercicio 01')
 '''
 Crea un programa que pida al usuario una contraseña, de forma repetitiva mientras que no introduzca “asdasd”. Cuando finalmente escriba la contraseña correcta, se le dirá “Bienvenido” y terminará elprograma.
---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Caso Practico Propuesto 3",
+    page_icon="👋"
+)
+st.title('Pagina Principal')
+st.sidebar.success('Selecciona una pagina.')
