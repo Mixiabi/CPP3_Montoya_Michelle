ed68b0d08a0ee0b3262eda0ddcfd56c10cb77e07
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-11-01 00:52:08 -0500

    Creando estilos para '*_ejercicios*.py'

---
+++ estilo.py
@@ -0,0 +1,35 @@
+pg_bg_pgs = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background: linear-gradient(
+        rgba(127, 255, 212, 1),
+        rgba(255, 218, 185, 1),
+        rgba(230, 230, 250, 1)
+    );
+    # background-size: cover;
+    # background-position: center;
+    # background-repeat: no-repeat;
+}
+[data-testid="stHeader"] {
+    background: linear-gradient(
+        rgba(127, 255, 212, 0.6),
+        rgba(200, 250, 200, 0.6)
+        );
+}
+[data-testid="stSidebarContent"] {
+    background: linear-gradient(to bottom, #7FFFD4, #FFDAB9, #E6E6FA);
+    color: black; 
+}
+[data-testid="stSidebarHeader"]::before {
+    content: "MENU PRINCIPAL";
+    font-weight: bold;
+    font-size: 18px;
+    color: black;
+}
+[data-testid="stSidebarNavItems"] {
+    font-weight: bold;
+    font-size: 18px;
+    color: black;
+}
+<style>
+"""
