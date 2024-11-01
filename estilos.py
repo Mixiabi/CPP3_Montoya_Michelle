c1c587dafae96c2b1e7acc40fc7ea45cbd2cfc17
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-11-01 00:33:16 -0500

    Agregando estilos background y sliderbar

---
+++ estilos.py
@@ -0,0 +1,29 @@
+pg_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)), 
+                      url('https://icontinental.edu.pe/wp-content/uploads/2023/05/logo-instituto-continental.png');
+    background-size: auto;
+    background-position: center;
+    background-repeat: no-repeat;
+    # height: 100vh; /* Ajusta la altura como necesites */
+    # width: 100vw;  /* Ajusta el ancho como necesites */
+    margin: auto; /* Centra el contenedor */
+}
+[data-testid="stSidebarContent"] {
+    background: linear-gradient(to bottom, #7FFFD4, #FFDAB9, #E6E6FA);
+    color: black; 
+}
+[data-testid="stSidebarHeader"]::before {
+    content: "MENU PRINCIPAL";
+    font-weight: bold;
+    font-size: 18px;
+    color: black;
+}
+[data-testid="stSidebarNavItems"] {
+    font-weight: bold;
+    font-size: 18px;
+    color: black;
+}
+<style>
+"""
