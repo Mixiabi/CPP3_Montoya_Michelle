0e6dd1609d2acf0c64dc25462c98b457739f05e1
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-11-01 00:34:24 -0500

    Importando "estilos.py" para formato de pagina

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+from estilos import pg_bg_img
 import streamlit as st
-# http://192.168.1.3:8501
+
 st.set_page_config(
     page_title="Caso Practico Propuesto 3",
     page_icon="📚"
 )
-st.title('👩🏻‍💻Fundamentos de Programacion 👋')
-st.sidebar.success('Selecciona una pagina.')
-st.header('📚CASO PRACTICO PROPUESTO 3')
 
+st.markdown(pg_bg_img,unsafe_allow_html=True)
+st.title('👩🏻‍💻Fundamentos de Programacion 👋')
+st.markdown("<br>", unsafe_allow_html=True) 
+st.header('📚CASO PRACTICO PROPUESTO 3')
+st.markdown("<br>", unsafe_allow_html=True) 
 st.header("📖**ALUMNO:**")
 st.subheader('Montoya Alvarado Michelle Abigail')
+st.markdown("<br>", unsafe_allow_html=True) 
 st.header("👨‍🏫**DOCENTE:**")
 st.subheader('Solorzano')
+st.markdown("<br>", unsafe_allow_html=True) 
 st.header("📅**AÑO:**")
 st.subheader('2024')
