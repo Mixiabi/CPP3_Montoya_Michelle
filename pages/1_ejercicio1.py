a6962677c1c2e50ebf4cd47283cc99dc2ac05b32
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-11-01 20:07:40 -0500

    Desarrollando ejercicio #5

--- pages/1_ejercicio1.py
+++ pages/1_ejercicio1.py
@@ -7,25 +7,18 @@ st.title('Ejercicio 01')
 Crea un programa que pida al usuario una contraseña, de forma repetitiva mientras que no introduzca “asdasd”. Cuando finalmente escriba la contraseña correcta, se le dirá “Bienvenido” y terminará elprograma.
 '''
 def get_password(usuario,contra):
-    
     if not usuario or not contra:
             return "El usuario y contraseña no deben estar vacios"
     while True: 
-            
         if contra == "asdasd":
             break
-        
         else:
             return "Contraseña incorrecta. vuelva a intentarlo"
 
-    
 def main():
     st.header('Inicio de Sesion')
-    
     usuario = st.text_input("**Usuario**")
-    
     contra = st.text_input("**Contraseña**", type='password')
-
     if st.button('Iniciar Sesion',type='primary'):
         error = get_password(usuario, contra)
         if error:
@@ -35,4 +28,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
--- pages/5_ejercicio5.py
+++ pages/5_ejercicio5.py
@@ -1,9 +1,57 @@
+import time
 import streamlit as st
 from estilo import pg_bg_pgs
 
 st.markdown(pg_bg_pgs,unsafe_allow_html=True)
 
-st.header('Ejercicio 05')
+st.title('Ejercicio 05')
 '''
+Algoritmo que pida números hasta que se introduzca un
+" 0 ". Debe imprimir la suma y la media de todos los números
+introducidos.
+'''
+def calculaLista(numero):
+    if 'numeros' not in st.session_state:
+        st.session_state['numeros'] = []
+    if not numero:
 
-'''
+        return "🚨 **Campo no debe estar vacío**"
+    try:
+        st.session_state['numeros'].append(numero)
+    except ValueError:
+        return 'Datos no validos'
+
+def reiniciar():
+    aviso = st.info('Calculo Reiniciado ...')
+    time.sleep(1.5)
+    aviso.empty()
+    st.session_state['numeros'].clear()
+
+def main():
+    st.header('Calcula Suma & Media de valores')
+    nuevoNumero = st.number_input('**Ingrese valores:**',value=None,format='%.f')
+
+    if nuevoNumero is None:
+        error = calculaLista(nuevoNumero)
+        if error:
+            st.error(error)
+
+    if st.button('**Reiniciar**',type='primary'):
+            reiniciar()
+            
+    if nuevoNumero != 0:
+        calculaLista(nuevoNumero)
+        st.write(f'Lista de números ingresados: {st.session_state['numeros']}')
+
+    else:
+        if st.session_state['numeros']:
+            suma = sum(st.session_state['numeros'])
+            media = suma / len(st.session_state['numeros'])
+            st.write(f'Lista de números ingresados: {st.session_state['numeros']}')
+            st.write(f'**Suma de los números:** {suma:.2f}')
+            st.write(f'**Media de los números:** {media:2f}')
+        else:
+            st.warning("🚨 **No hay números para calcular**")
+
+if __name__ == "__main__":
+    main()
